effd3cfadc17a873e57b0cd8d6da234c3b14ac18
Tommassino <batsup@gmail.com>
2017-05-14 16:39:22 +0200

    add route configs

--- client/config/dev.env.js
+++ client/config/dev.env.js
@@ -1,5 +1,6 @@
 module.exports = {
   NODE_ENV: '"development"',
-  bind_host: "localhost",
-  bind_port: "3000"
+  bind_host: "riplomacy.witzany.cz",
+  bind_port: "80",
+  bind_path: "/api/socket.io"
 };
--- client/main.js
+++ client/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import SocketIO from 'socket.io-client'
 import VueSocketIO from 'vue-socket.io';
 import VueRouter from 'vue-router';
 import store from './vuex/vuex-store.js'
@@ -9,7 +10,8 @@ import OpDetail from './components/OpDetail.vue'
 
 import config from './config/dev.env.js'
 
-Vue.use(VueSocketIO, 'http://'+config.bind_host+":"+config.bind_port, store);
+var socketio = SocketIO('http://'+config.bind_host+":"+config.bind_port, {path:config.bind_path});
+Vue.use(VueSocketIO, socketio, store);
 Vue.use(VueRouter);
 
 const router = new VueRouter({
